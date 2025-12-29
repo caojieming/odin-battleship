c4d0ba67391b93010485ce52321934e845649c23
James Cao <jcao5360@gmail.com>
2025-12-29 16:02:06 -0500

    preparing for future removal

--- src/views/view.js
+++ src/views/view.js
@@ -1,7 +1,5 @@
 import exampleImage from "../assets/img/example.jpeg";
 
 export function createImg() {
-    const image = document.createElement("img");
-    image.src = exampleImage;
-    document.querySelector("#content").appendChild(image);
+    
 }
